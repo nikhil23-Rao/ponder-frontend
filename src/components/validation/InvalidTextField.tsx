ecd638f6625ceedca9da8a8548dc277099c3a0c8
Nikhil <nikhil23.rao@gmail.com>
2021-04-07 20:41:14 -0700

    try netlif.toml

--- src/components/validation/InvalidTextField.tsx
+++ src/components/validation/InvalidTextField.tsx
@@ -9,7 +9,7 @@ export const InvalidTextField: React.FC<FieldAttributes<{}>> = ({
   const [field] = useField(props);
   return (
     <TextField
-      style={{ width: "300px" }}
+      style={{ width: "350px" }}
       placeholder={placeholder}
       {...field}
       variant="outlined"
